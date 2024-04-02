12abee503f043d0d6f7a434ef23070d297a58d4c
christian-grund <151537557+christian-grund@users.noreply.github.com>
2024-04-02 09:30:40 +0200

    54 - audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,8 @@
 let mute = false;
 
+/**
+ * Array containing audio data for various game sounds.
+ */
 let audioData = [
   new Audio('audio/del-rio-bravo.mp3'),
   new Audio('audio/game_lost.mp3'),
@@ -23,6 +26,9 @@ let audioData = [
   new Audio('audio/backgroundsong.mp3'),
 ];
 
+/**
+ * Toggles the mute state of the game.
+ */
 function toggleMuteButton() {
   let soundButton = document.getElementById('mute-button');
 
@@ -37,6 +43,9 @@ function toggleMuteButton() {
   soundButton.blur();
 }
 
+/**
+ * Mutes all game sounds.
+ */
 function muteSound() {
   audioData.forEach((audio) => {
     audio.muted = true;
@@ -45,6 +54,9 @@ function muteSound() {
   mute = true;
 }
 
+/**
+ * Unmutes all game sounds.
+ */
 function unmuteSound() {
   audioData.forEach((audio) => {
     audio.muted = false;
@@ -52,114 +64,195 @@ function unmuteSound() {
   mute = false;
 }
 
+/**
+ * Plays the start screen background sound.
+ */
 function playStartScreenSound() {
-  // audioData[0].volume = 0.5;
+  audioData[0].volume = 0.5;
   audioData[0].play();
 }
 
+/**
+ * Pauses the start screen background sound.
+ */
 function pauseStartScreenSound() {
   audioData[0].pause();
 }
 
+/**
+ * Plays the sound effect for when the game is lost.
+ */
 function playGameLostSound() {
   audioData[1].play();
 }
 
+/**
+ * Pauses the game lost sound.
+ */
 function pauseGameLostSound() {
   audioData[1].pause();
 }
 
+/**
+ * Plays the sound effect for coin collection.
+ */
 function playCoinCollectSound() {
   audioData[2].cloneNode(true).play();
 }
 
+/**
+ * Plays the sound effect for bottle collection.
+ */
 function playBottleCollectSound() {
   audioData[3].cloneNode(true).play();
 }
 
+/**
+ * Plays the sound effect for bottle refill.
+ */
 function playBottleRefillSound() {
   audioData[4].play();
 }
 
+/**
+ * Plays the walking sound effect.
+ */
 function playWalkingSound() {
   audioData[5].play();
 }
 
+/**
+ * Pauses the walking sound effect.
+ */
 function pauseWalkingSound() {
   audioData[5].pause();
 }
 
+/**
+ * Plays the jumping sound effect.
+ */
 function playJumpingSound() {
   audioData[6].play();
 }
 
+/**
+ * Plays the hurt sound effect.
+ */
 function playHurtSound() {
   audioData[7].play();
 }
 
+/**
+ * Plays the snoring sound effect.
+ */
 function playSnoringSound() {
   audioData[8].play();
 }
 
+/**
+ * Pauses the snoring sound effect.
+ */
 function pauseSnoringSound() {
   audioData[8].pause();
 }
 
+/**
+ * Plays the throwing sound effect.
+ */
 function playThrowingSound() {
   audioData[9].play();
 }
 
+/**
+ * Plays the bottle splash sound effect.
+ */
 function playBottleSplashSound() {
   audioData[10].play();
 }
 
+/**
+ * Plays the sound effect for normal chicken defeated.
+ */
 function playNormalChickenDefeatedSound() {
   audioData[11].play();
 }
 
+/**
+ * Plays the sound effect for small chicken defeated.
+ */
 function playSmallChickenDefeatedSound() {
   audioData[12].play();
 }
 
+/**
+ * Plays the sound effect for endboss attack.
+ */
 function playEndossAttakSound() {
   audioData[13].play();
 }
 
+/**
+ * Plays the sound effect for endboss hurt.
+ */
 function playEndbossHurtSound() {
   audioData[14].play();
 }
 
+/**
+ * Plays the sound effect for endboss defeated.
+ */
 function playEndbossDefeatedSound() {
   audioData[15].play();
 }
 
+/**
+ * Plays the sound effect for game won.
+ */
 function playGameWonSound() {
   audioData[16].play();
 }
 
+/**
+ * Plays the sound effect for endboss attack.
+ */
 function playEndbossAttakSound() {
   audioData[17].play();
 }
 
+/**
+ * Pauses the sound effect for endboss attack.
+ */
 function pauseEndbossAttakSound() {
   audioData[17].pause();
 }
 
+/**
+ * Plays the sound effect for endboss fight.
+ */
 function playEndbossFightSound() {
   let audio = audioData[18];
   audio.loop = true;
   audio.play();
 }
 
+/**
+ * Pauses the sound effect for endboss fight.
+ */
 function pauseEndbossFightSound() {
   audioData[18].pause();
 }
 
+/**
+ * Plays the background game sound.
+ */
 function playGameSound() {
   audioData[19].volume = 0.25;
   audioData[19].play();
 }
 
+/**
+ * Pauses the background game sound.
+ */
 function pauseGameSound() {
   audioData[19].pause();
 }
